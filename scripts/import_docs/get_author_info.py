340e8eca92d44e72f97aca575158ff47944111fe
aiborg <lean-architect@proton.me>
2025-01-29 05:29:25 +0000

    feat: add scripts for automated git operations and author management in document import process

---
+++ scripts/import_docs/get_author_info.py
@@ -0,0 +1,11 @@
+#!/bin/env python3
+
+import sys
+
+authors = sys.argv[1]
+
+if authors == "0KYuINCm0LXRgNC10L3QvtCy":
+    print("Tseren Tserenov:tserenov@aisystant.com")
+    sys.exit(0)
+
+print("Aisystant:bots@aisystant.com")
